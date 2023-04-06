1da8b188fe0389934d53cf426d424a3c93f5b18b
kimdaeun <onlydk57@gmail.com>
2023-04-06 17:53:19 +0900

    변수 - 자바의 기본적인 코드 구성에서 각 키워드 설명

---
+++ src/Variables/HelloWorldExplain.java
@@ -0,0 +1,16 @@
+package Variables;
+// 자바의 기본적인 코드 구성에서 각 키워드를 간략히 설명해보시오.
+public class HelloWorldExplain {
+    public static void main(String[] args) {
+        System.out.println("Hello World~");
+    }
+}
+
+/*접근제어자 클래스 선언 클래스 이름{
+    접근제어자 스태틱 반환타입 메서드이름(매개변수/파라미터)}
+        구현코드
+    }
+}
+
+접근제어자 -> 클래스나 메서드에 접근할 수 있는 범위를 지정하는 제어자
+*/
